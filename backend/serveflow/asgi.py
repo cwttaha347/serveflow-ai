aedba5ffc49ec798575eef7cef41c4266e4ca083
daniellopez882 <daniellopezorta39@gmail.com>
2026-04-24 02:15:40 +0500

    Fix ASGI initialization order (AppRegistryNotReady)

--- backend/serveflow/asgi.py
+++ backend/serveflow/asgi.py
@@ -8,17 +8,18 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "serveflow.settings")
+django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
 from api.middleware_channels import TokenAuthMiddleware
 import api.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "serveflow.settings")
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         TokenAuthMiddleware(
             URLRouter(
@@ -27,3 +28,4 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+
